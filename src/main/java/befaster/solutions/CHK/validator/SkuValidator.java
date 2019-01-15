9f5455b3a0951b411a23142875fe3cd58e45b21d
sbx_user1060 <sbx_user1060@ip-10-129-153-32.eu-west-2.compute.internal>
2019-01-15 20:27:40 +0000

    Tue Jan 15 20:27:40 UTC 2019

--- src/main/java/befaster/solutions/CHK/validator/SkuValidator.java
+++ src/main/java/befaster/solutions/CHK/validator/SkuValidator.java
@@ -1,15 +1,28 @@
 package befaster.solutions.CHK.validator;
 
+import org.apache.commons.collections4.CollectionUtils;
+
 import java.util.List;
 
 public class SkuValidator {
 
+    private Enum itemCode {
+
+    }
+
     public boolean validate(List<String> input ){
 
-        if(CollectionUtils)
+        if(CollectionUtils.isEmpty(input)){
+            // shouldn't happen...
+            return false;
+        }
+
+        input.stream().
+
         return false;   //TODO
     }
 
 
 }
+
 

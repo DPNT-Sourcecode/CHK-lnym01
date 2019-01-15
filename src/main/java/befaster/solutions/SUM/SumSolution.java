d18b57d717ceb280463cc7d422b5d1aecf16efb1
sbx_user1060 <sbx_user1060@ip-10-129-153-32.eu-west-2.compute.internal>
2019-01-15 19:14:31 +0000

    Tue Jan 15 19:14:31 UTC 2019

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -2,16 +2,28 @@ package befaster.solutions.SUM;
 
 public class SumSolution {
 
+    private static final int MIN = 0;
+
+    private static final int MAX = 100;
+
     public Integer compute(Integer x, Integer y) {
 
         if(x == null || y == null){
-            throw new IllegalArgumentException("compute: cannot sum null parameters");
+            throw new IllegalArgumentException("compute: one or more parameters is null");
+        }
+        if(! parameterWithinRange(x) ||  !parameterWithinRange(y)){
+            throw new IllegalArgumentException("compute: one or more parameters exceed min/max range");
         }
 
         return x + y;
     }
 
+    private boolean parameterWithinRange(int input){
+
+        if(input < MIN || input > MAX){
+            return false;
+        }
+        return true;
+    }
+
 }
-
-
-

5913562f4fcf96a1e6e1c871fd3e58de5c6bdf0d
sbx_user1060 <sbx_user1060@ip-10-129-153-32.eu-west-2.compute.internal>
2019-01-15 20:03:40 +0000

    Tue Jan 15 20:03:40 UTC 2019

--- src/main/java/befaster/solutions/CHK/model/Item.java
+++ src/main/java/befaster/solutions/CHK/model/Item.java
@@ -1,4 +1,25 @@
 package befaster.solutions.CHK.model;
 
 public class Item {
+
+    private String id;
+
+    private int price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
+
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,4 +1,33 @@
 package befaster.solutions.CHK;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class CheckoutSolutionTest {
+
+    private static final Integer ILLEGAL_INPUT_RESULT = -1;
+
+    private CheckoutSolution subject;
+
+    @Before
+    public void setup(){
+        subject = new CheckoutSolution();
+    }
+
+    @Test
+    public void illegal_input_returns_minus_one(){
+
+        // given:
+        String input = ",vfmldkbndlkfgmds";
+
+        // when:
+        Integer result = subject.checkout(input);
+
+        // then:
+        assertThat(result, is(ILLEGAL_INPUT_RESULT));
+    }
 }
+
